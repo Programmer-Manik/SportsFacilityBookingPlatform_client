2ad848115002a2f9cd47d4e33c318b8a8ffcc041
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-08-26 10:27:33 +0600

    testomonials section completed

--- src/pages/Landing/LandingPages.tsx
+++ src/pages/Landing/LandingPages.tsx
@@ -1,9 +1,19 @@
+import FeaturesFacilities from "./features/FeaturesFacilities";
 import Header from "./Headers/Header";
+import Services from "./Services/Services";
+import SiteBanner from "./siteBanner/SiteBanner";
+import Testimonials from "./Testimonials/Testimonials";
+import HowItWorks from "./Works/HowItWorks";
 
 const LandingPages = () => {
   return (
     <div>
       <Header />
+      <HowItWorks />
+      <FeaturesFacilities />
+      <Services />
+      <SiteBanner />
+      <Testimonials />
     </div>
   );
 };
